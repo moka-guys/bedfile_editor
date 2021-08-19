f28a8d0771ea8e2709b9be43b6821c0188ebcaac
Graeme <graeme_c_smith@hotmail.com>
2021-08-19 22:47:48 +0100

    Added initial models for BedfileRequest and Gene

--- bed_maker/models.py
+++ bed_maker/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class BedfileRequest(models.Model):
+    """
+    Model to store bedfile request details
+    """
+    bedfile_request_id = models.AutoField(primary_key=True)
+    pan_number = models.CharField(max_length=10)
+    date_requested = models.DateField()
+    request_status = models.CharField(max_length=10, choices=(('draft', 'draft'), ('published', 'published')))
+
+
+class Gene(models.Model):
+    """
+    Model to store gene details
+    """
+    gene_id = models.AutoField(primary_key=True)
+    # ensembl gene ID begining ENSG
+    ensembl_gene_id = models.CharField(max_length=15, primary_key=True)
+    bedfile_request_id = models.ForeignKey('Gene', on_delete=models.CASCADE)
