6de4d58df3de0d0a7fedde64449625f8c5060464
Graeme <graeme_c_smith@hotmail.com>
2021-08-20 09:57:28 +0100

    Used crispy forms package to format import form nicely

--- bed_maker/forms.py
+++ bed_maker/forms.py
@@ -1,12 +1,35 @@
 from django import forms
 from .models import  BedfileRequest
 
+from django.urls import reverse
+from crispy_forms.bootstrap import Field
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit, Layout, HTML
 
 class ManualUploadForm(forms.Form):
     """
+    Form for manually inputting data
     """
 
     pan_number = forms.CharField()
     date_requested = forms.DateField()
     requested_by = forms.CharField()
     
+    def __init__(self, *args, **kwargs):
+        super(ManualUploadForm, self).__init__(*args, **kwargs)
+
+        self.helper = FormHelper()
+        self.helper.form_id = 'manual-upload-form'
+        self.helper.label_class = 'col-lg-2'
+        self.helper.field_class = 'col-lg-10'
+        self.helper.form_method = 'post'
+        self.helper.form_action = reverse('view')
+        self.helper.add_input(Submit('submit', 'Import', css_class='btn-success'))
+        self.helper.form_class = 'form-horizontal'
+        self.helper.layout = Layout(
+            HTML('<br><h5>Bedfile Request</h5>'),
+            Field('pan_number'),
+            Field('date_requested'),
+            Field('requested_by'),
+            HTML('<br><h5>Ensembl Gene List</h5>'),
+        )   
--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bed_maker',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
