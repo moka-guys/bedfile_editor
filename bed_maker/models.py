5a59690beb81741260fa306fdb7a058538e0d61e
Graeme <graeme_c_smith@hotmail.com>
2021-08-20 17:10:43 +0100

    Added transcript model to database

--- bed_maker/models.py
+++ bed_maker/models.py
@@ -21,3 +21,19 @@ class Gene(models.Model):
     # ensembl gene ID begining ENSG
     ensembl_gene_id = models.CharField(max_length=15)
     bedfile_request_id = models.ForeignKey('BedfileRequest', on_delete=models.CASCADE)
+
+class Transcript(models.Model):
+    """
+    Model to store transcript details
+    """
+    transcript_id = models.AutoField(primary_key=True)
+    # ensembl transcript ID begining ENST
+    ensembl_transcript_id = models.CharField(max_length=15)
+    # RefSeq transcript ID begining NM
+    RefSeq_transcript_id = models.CharField(max_length=15)
+    bedfile_request_id = models.ForeignKey('BedfileRequest', on_delete=models.CASCADE)
+    ensembl_gene_id = models.ForeignKey('Gene', on_delete=models.CASCADE)
+    display_name = models.CharField(max_length=15)
+    start = models.CharField(max_length=50)
+    end = models.CharField(max_length=50)
+    MANE_transcript = models.BooleanField()
